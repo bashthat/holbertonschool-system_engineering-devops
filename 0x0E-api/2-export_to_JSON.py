cbc6c71622edb341bddbdcdb701fb5975ed64ece
bashthat <3249@holbertonschool.com>
2022-08-01 22:21:00 -0500

    syntax

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -11,26 +11,26 @@ import sys
 import json
 
 if __name__ == '__main__':
-        uid = sys.argv[1]  # too many comments
-        url = "https://jsonplaceholder.typicode.com/"
-        user = "{}users/{}".format(url, uid)
-        req = requests.get(user)
-        jx = req.json()
-        name = jx.get('username')
-        todos = "{}todos?uid={}".format(url, uid)
-        req = requests.get(todos)
-        jx = req.json()
-        xyz = []
-        for task in jx:
-            rnr = [{"task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": name}]
-            xyz.append(rnr)  # append the username to the task
-        """
+    uid = sys.argv[1]  # too many comments
+    url = "https://jsonplaceholder.typicode.com/"
+    user = "{}users/{}".format(url, uid)
+    req = requests.get(user)
+    jx = req.json()
+    name = jx.get('username')
+    todos = "{}todos?uid={}".format(url, uid)
+    req = requests.get(todos)
+    jx = req.json()
+    xyz = []
+    for task in jx:
+        rnr = [{"task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": name}]
+        xyz.append(rnr)  # append the username to the task
+    """
         jsonify the tasks
         """
-        rnr = {}
-        rnr[uid] = xyz
-        
-        with open("{}.json".format(uid), mode='w') as f:
-            json.dump(rnr, f)
+    rnr = {}
+    rnr[uid] = xyz
+
+    with open("{}.json".format(uid), mode='w') as f:
+        json.dump(rnr, f)
