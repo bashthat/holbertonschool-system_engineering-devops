8909751bac4f8ec49a5ae0006f1842ec91b30662
bashthat <3249@holbertonschool.com>
2022-08-01 19:22:18 -0500

    0pyllint

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,22 +2,21 @@
 """
 extend your Python script to export data in the JSON format.
 """
-
-import requests
 import json
-from sys import argv
+import requests
+
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    req = requests.get('{}users'.format(url)).json()
+    URL = 'https://jsonplaceholder.typicode.com/'
+    req = requests.get('{}users'.format(URL)).json()
     xyz = {}
     for user in req:
         Id = user.get('id')  # request id
         username = user.get('username')  # username
-        todos = requests.get('{}users/{}/todos'.format(url,
+        todos = requests.get('{}users/{}/todos'.format(URL,
                              Id)).json()  # jsonify todos
         tsk = [{'username': username, 'task': tsk.get('title'),
                 'completed': tsk.get('completed')} for tsk in todos]
         xyz[Id] = tsk  # the tasks given the format to the problem
-    with open('todo_all_employees.json', 'w') as jsn:
+    with open("todo_all_employees.json", "w") as jsn:
         json.dump(xyz, jsn)  # word
